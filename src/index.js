cc94b8ab737f56c9468b304f21eb646a1f1b4627
lujiewen <jevense@qq.com>
2018-03-09 11:12:44 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-import Section from './App.vue'
+import Section from './App.vue';
 
-export default Section
+export default Section;
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.component('imed-section', Section);
+}
